7d0a4403e664cd854ddc4070d633280303da8acd
Hussein-02 <hussein02ab@gmail.com>
2025-03-24 03:15:16 +0200

    Finished migration for onboarding tasks table

--- Server/database/migrations/2025_03_24_011429_create_onboarding_tasks_table.php
+++ Server/database/migrations/2025_03_24_011429_create_onboarding_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('onboarding_tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('onboarding_id')->constrained('onboardings')->onDelete('cascade');
+            $table->string('title', 255);
+            $table->enum('status', ['pending', 'completed']);
             $table->timestamps();
         });
     }
