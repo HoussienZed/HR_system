e3ef004e9682f101e98322ee1c2ab92536b7875d
Hussein-02 <hussein02ab@gmail.com>
2025-03-24 02:58:31 +0200

    Finished migration for courses table

--- Server/database/migrations/2025_03_24_005654_create_courses_table.php
+++ Server/database/migrations/2025_03_24_005654_create_courses_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->string('pdf_url', 255);
+            $table->string('course_category', 255);
             $table->timestamps();
         });
     }
