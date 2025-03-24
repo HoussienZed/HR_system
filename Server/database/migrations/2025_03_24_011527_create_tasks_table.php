06502c1492b471eaa5b09d10fdba53d710f4f45b
Hussein-02 <hussein02ab@gmail.com>
2025-03-24 03:16:11 +0200

    Finished migration for tasks table

--- Server/database/migrations/2025_03_24_011527_create_tasks_table.php
+++ Server/database/migrations/2025_03_24_011527_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('title', 255);
+            $table->enum('status', ['to do', 'in progress', 'completed']);
             $table->timestamps();
         });
     }
