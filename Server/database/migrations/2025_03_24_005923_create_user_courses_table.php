c51f1acd4290d06bd93bbe0843c93877525a5da7
Hussein-02 <hussein02ab@gmail.com>
2025-03-24 03:02:17 +0200

    Finished migration for user courses table

--- Server/database/migrations/2025_03_24_005923_create_user_courses_table.php
+++ Server/database/migrations/2025_03_24_005923_create_user_courses_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('user_courses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->timestamp('started_at')->useCurrent();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamp('certificate_issued_at')->nullable();
+            $table->enum('status', ['started', 'completed', 'certificate_issued'])->default('started');
             $table->timestamps();
         });
     }
