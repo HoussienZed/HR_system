b948484472523761be33944331a57d17d00a51c3
Houssien Zeineddine <houssienzd@hotmail.com>
2025-03-24 02:56:42 +0200

    finished side navbar component and styling it, importing font family

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Users from "./assets/component/users-table";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter></BrowserRouter>
+    </>
   );
 };
 
