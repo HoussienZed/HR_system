04cc25f9726b982cc7000638f172ab86d7ef9f2d
Hussein-02 <hussein02ab@gmail.com>
2025-03-24 03:04:26 +0200

    Finished migration for benefits table

--- Server/database/migrations/2025_03_24_010338_create_benefits_table.php
+++ Server/database/migrations/2025_03_24_010338_create_benefits_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('benefits', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->decimal('experience_bonus', 10, 2)->default(0);
+            $table->decimal('special_allowance', 10, 2)->default(0);
+            $table->decimal('appraisal', 10, 2)->default(0);
             $table->timestamps();
         });
     }
