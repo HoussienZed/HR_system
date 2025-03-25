fa4c50cdd08e63f0c6a1dbdbca10a233741e8d49
Hussein-02 <hussein02ab@gmail.com>
2025-03-25 02:31:34 +0200

    Finished interview model

--- Server/app/Models/Interview.php
+++ Server/app/Models/Interview.php
@@ -9,4 +9,11 @@ class Interview extends Model
 {
     /** @use HasFactory<\Database\Factories\InterviewFactory> */
     use HasFactory;
+
+    protected $fillable = ['candidate_id', 'scheduled_date', 'feedback', 'status'];
+
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class);
+    }
 }
