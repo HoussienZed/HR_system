daced0ca26c8d6b393b351d8b1c8fa224d8a82ce
Hussein-02 <hussein02ab@gmail.com>
2025-03-25 02:01:42 +0200

    Added eloquent relation between

--- Server/app/Models/AIQuery.php
+++ Server/app/Models/AIQuery.php
@@ -9,4 +9,9 @@ class AIQuery extends Model
 {
     /** @use HasFactory<\Database\Factories\AIQueryFactory> */
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
