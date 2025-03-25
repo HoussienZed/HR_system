4ec32c33082b07c4c6917566090726f7acbd3716
Hussein-02 <hussein02ab@gmail.com>
2025-03-25 13:38:04 +0200

    Finished Seeders

--- Server/database/seeders/AIQuerySeeder.php
+++ Server/database/seeders/AIQuerySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AIQuery;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class AIQuerySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        AIQuery::factory(10)->create();
     }
 }
--- Server/database/seeders/OnboardingSeeder.php
+++ Server/database/seeders/OnboardingSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Onboarding;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class OnboardingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Onboarding::factory(5)->create();
     }
 }
