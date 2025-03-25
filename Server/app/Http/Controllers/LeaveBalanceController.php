205755617069de467b83d710d0416256358852ce
Hussein-02 <hussein02ab@gmail.com>
2025-03-25 23:37:45 +0200

    created get user leave balance function

--- Server/app/Http/Controllers/LeaveBalanceController.php
+++ Server/app/Http/Controllers/LeaveBalanceController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LeaveBalance;
 use Illuminate\Http\Request;
 
 class LeaveBalanceController extends Controller
 {
-    //
+    public function getUserLeaveBalance($userId)
+    {
+        $leaveBalance = LeaveBalance::where('user_id', $userId)->get();
+        return response()->json($leaveBalance);
+    }
 }
