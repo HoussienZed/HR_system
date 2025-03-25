16a7f506ef5c9a943de9d9fb7ba2691a8e7a8a4e
Hussein-02 <hussein02ab@gmail.com>
2025-03-25 02:33:09 +0200

    Finished task model

--- Server/app/Models/Task.php
+++ Server/app/Models/Task.php
@@ -9,4 +9,11 @@ class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'description', 'status', 'deadline'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
