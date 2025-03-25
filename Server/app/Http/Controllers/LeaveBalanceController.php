165983994edf1774237a7128b87fb0bd32b50c2a
Hussein-02 <hussein02ab@gmail.com>
2025-03-25 23:48:21 +0200

    Finished leave balance controller

--- Server/app/Http/Controllers/LeaveBalanceController.php
+++ Server/app/Http/Controllers/LeaveBalanceController.php
@@ -18,4 +18,19 @@ class LeaveBalanceController extends Controller
         $leavesBalances = LeaveBalance::with('user')->get();
         return messageResponse(true, 'Fetched all users leave balance successfully', 201, $leavesBalances);
     }
+
+    public function getTotalBalanceByType()
+    {
+        $totalBalances = LeaveBalance::selectRaw('leave_type, SUM(balance) as total_balance')
+            ->groupBy('leave_type')
+            ->get();
+
+        return messageResponse(true, 'Fetched total balance by type successfully', 201, $totalBalances);
+    }
+
+    public function getOverallTotalBalance()
+    {
+        $totalBalance = LeaveBalance::sum('balance');
+        return messageResponse(true, 'Fetched overall total balance successfully', 201, $totalBalance);
+    }
 }
