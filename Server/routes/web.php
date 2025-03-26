fe65f6c322445685e2f4725573c225548d2e9ce8
Riyad Murad <riyadmurad01@gmail.com>
2025-03-27 01:28:42 +0200

    added a function to web.api

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/', function () {
+    return redirect('/api/v1'); // Or some other meaningful response
+});
