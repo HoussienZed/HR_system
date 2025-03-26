6999d3d45c085c6b9c571f490c51a69022b2650e
Hussein-02 <hussein02ab@gmail.com>
2025-03-26 13:00:46 +0200

    Finished leave request page on frontend

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,6 +9,7 @@ import BenefitsDashboard from "./pages/employee/BenefitsDashboard";
 import EditBenefits from "./pages/employee/EditBenefits";
 import RetirementCalculator from "./pages/employee/RetirementCalculator";
 import Attendance from "./pages/employee/Attendance";
+import LeaveRequest from "./pages/employee/LeaveRequest/index";
 
 const App = () => {
   const hrSidebarItems = [
@@ -65,15 +66,10 @@ const App = () => {
             >
               <Route path="employeepayroll" element={<EmployeePayroll />} />
               <Route path="/attendance" element={<Attendance />} />
-              <Route
-                path="/benefitsdashboard"
-                element={<BenefitsDashboard />}
-              />
+              <Route path="/benefitsdashboard" element={<BenefitsDashboard />} />
               <Route path="/editbenefits" element={<EditBenefits />} />
-              <Route
-                path="/retirementcalculator"
-                element={<RetirementCalculator />}
-              />
+              <Route path="/retirementcalculator" element={<RetirementCalculator />} />
+              <Route path="/leaveRequest" element={<LeaveRequest />} />
             </Route>
 
             {/*routes for HRs access only */}
@@ -86,10 +82,7 @@ const App = () => {
                   </>
                 }
               >
-                <Route
-                  path="/payrolldashboard"
-                  element={<PayrollDashboard />}
-                ></Route>
+                <Route path="/payrolldashboard" element={<PayrollDashboard />}></Route>
               </Route>
             </Route>
           </Route>
