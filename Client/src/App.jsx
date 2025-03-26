f0286b7c22905e0d759bbf51cbae5a102aaf4e48
Riyad Murad <riyadmurad01@gmail.com>
2025-03-26 23:18:09 +0200

    Resolved an error: fixed file path error

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,7 @@ import EditBenefits from "./pages/employee/EditBenefits";
 import RetirementCalculator from "./pages/employee/RetirementCalculator";
 import Attendance from "./pages/employee/Attendance";
 import { getUserType } from "./utils/getUserType";
-import Employees from "./pages/hr/Employees";
+import Employees from "./pages/Hr/Employees";
 
 const App = () => {
   const type = getUserType();
--- Server/routes/api.php
+++ Server/routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\RemoteWorkLocationController;
 
-Route::group(['prefix' => 'v1'], function () {
+Route::group(['prefix' => 'v1'], function(){
     //Authorized Users
     Route::group(["middleware" => "auth:api"], function () {
         //Authorized Users (HR)
@@ -14,18 +14,19 @@ Route::group(['prefix' => 'v1'], function () {
             // Route::get('/dashboard', [DashboardController::class, "dashboard"]);
         });
         
-        //Authorized Users
+        //Unauthorized Users
         Route::group(["prefix" => "Employees"], function () {
             Route::post('/clockIn', [AttendanceController::class, "clockIn"]);
             Route::post('/clockOut', [AttendanceController::class, "clockOut"]);
             Route::post('/addRemoteLocation', [RemoteWorkLocationController::class, "addRemoteLocation"]);
-
         });
+
     });
 
     //Unauthenticated Users
     Route::post('/login', [AuthController::class, "login"])->name('login');
     Route::post('/signup', [AuthController::class, "signup"])->name('signup');
+
 });
 
 // Route::post('/login', [AuthController::class, "login"])->name('login');
