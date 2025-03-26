b2d32d20c97a4b5f7ee0b5d2efab6bad2d2909b7
Hussein-02 <hussein02ab@gmail.com>
2025-03-26 16:31:09 +0200

    Added an event listener such that when a leave request is approved the it gets deducted from users leave balance

---
+++ Server/app/Listeners/DeductLeaveBalance.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Listeners;
+
+
+use App\Events\LeaveRequestsApproved;
+use App\Models\LeaveBalance;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class DeductLeaveBalance
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(LeaveRequestsApproved $event): void
+    {
+        $leaveRequest = $event->leaveRequest;
+
+        //get the number of days for the leave
+        $startDate = new \Carbon\Carbon($leaveRequest->start_date);
+        $endDate = new \Carbon\Carbon($leaveRequest->end_date);
+        $requestedDays = $startDate->diffInDays($endDate);
+
+        //find the record of the user's leave balance
+        $leaveBalance = LeaveBalance::where('user_id', $leaveRequest->user_id)
+            ->where('leave_type', $leaveRequest->leave_type)
+            ->first();
+
+        //subtract the number of days
+        if ($leaveBalance) {
+            $leaveBalance->balance -= $requestedDays;
+            $leaveBalance->save();
+        }
+    }
+}
