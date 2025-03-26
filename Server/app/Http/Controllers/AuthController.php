69d3fd5b6fd776724eb7a6c333ae97ef0745083c
Hussein-02 <hussein02ab@gmail.com>
2025-03-26 16:17:50 +0200

    Added an event listener that when a user is created his balance gets created in the leavebalance table

--- Server/app/Http/Controllers/AuthController.php
+++ Server/app/Http/Controllers/AuthController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\UserRegistered;
 use Throwable;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller{
-    public function login(Request $request){
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
 
         $request->validate([
             'email' => 'required|string|email',
@@ -18,14 +21,13 @@ class AuthController extends Controller{
 
         $token = Auth::attempt($credentials);
         if (!$token) {
-            return errorMessageResponse(false, "Access Error", "Unauthorized Access", 401);            
+            return errorMessageResponse(false, "Access Error", "Unauthorized Access", 401);
         }
 
         $user = Auth::user();
-        $user->token=$token;
+        $user->token = $token;
         // die($user);
         return messageResponse(true, "Login Successful", 200, $user);
-
     }
 
     function signup(Request $request){
@@ -37,6 +39,16 @@ class AuthController extends Controller{
             ]);
     
             $user = new User; 
+    function signup(Request $request)
+    {
+        try {
+            // $request->validate([
+            //     'full-name' => 'required|string|max:30',
+            //     'email' => 'required|string|email|max:255|unique:users',
+            //     'password' => 'required|string|min:8',
+            // ]);
+
+            $user = new User;
             // $user->name = $request["full-name"];
             $user->full_name = $request["full_name"];
             $user->email = $request["email"];
@@ -46,19 +58,22 @@ class AuthController extends Controller{
             $user->medical_plan_id = 1;
             $user->save();
             $user->token = Auth::login($user);
+            event(new UserRegistered($user));
             return messageResponse(true, "User created successfully", 200, $user);
         } catch (Throwable $e) {
             return errorMessageResponse(false, "False credentials", $e->getMessage(), 401);
         }
     }
 
-    public function logout(){
+    public function logout()
+    {
 
         Auth::logout();
         return messageResponse(true, "Successfully logged out", 200);
     }
 
-    public function refresh(){
+    public function refresh()
+    {
 
         $token = Auth::refresh();
         $user = Auth::user();
