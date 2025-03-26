bc9e7929656cccaae04cb8cea27fefea1508bd83
Riyad Murad <riyadmurad01@gmail.com>
2025-03-26 23:52:13 +0200

    Resolved an error: fixed axios base url and .env configurations

--- Client/src/utils/axios.js
+++ Client/src/utils/axios.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 
 const axiosBaseUrl = axios.create({
   // baseURL: "http://localhost:8000/api/v1",
-  baseURL: "http://52.47.74.242/snippet-server/api/v1",
+  baseURL: "http://15.188.49.159/hr-server/api/v1",
 
   headers: {
     "Content-Type": "application/json",
