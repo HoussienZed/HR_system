42ab63666eb28a458fd64a056c263a74ecdc53d0
Houssien Zeineddine <houssienzd@hotmail.com>
2025-03-26 10:11:26 +0200

    finished the skeleton of the retirement calculator

--- Client/src/pages/employee/RetirementCalculator.jsx
+++ Client/src/pages/employee/RetirementCalculator.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import accountingSvg from "../../assets/accounting.svg";
+import Input from "../../component/others/Input";
+import "../../assets/styles/retirementCalculator.css";
+import Button from "../../component/others/Button";
 
 const RetirementCalculator = () => {
-  return <div>RetirementCalculator</div>;
+  const [workStartingDate, setWorkStartingDate] = useState();
+  const [workEndingDate, setWorkEndingDate] = useState();
+  const [lastBasicSalary, setLastBasicSalary] = useState();
+  const [retirementAmount, setRetirementAmount] = useState("");
+
+  const handleRetirementCalculation = () => {
+    setRetirementAmount();
+  };
+
+  return (
+    <div className="retirement-calculator-container">
+      <div className="inputs-svg-container flex gap-8">
+        <div className="flex flex-column items-center gap-4 inputs-container">
+          <h1>Retirment Calculator</h1>
+          <Input
+            type={"text"}
+            name={"workStartingDate"}
+            value={workStartingDate}
+            onChange={(e) => setWorkStartingDate(e.target.value)}
+            placeholder={"Enter your work starting date"}
+            className={"body2 rounded-lg calculator-input"}
+          />
+          <Input
+            type={"text"}
+            name={"workEndingDate"}
+            value={workEndingDate}
+            onChange={(e) => setWorkEndingDate(e.target.value)}
+            placeholder={"Enter your work ending date"}
+            className={"body2 rounded-lg calculator-input"}
+          />
+          <Input
+            type={"text"}
+            name={"lastBasicSalary"}
+            value={lastBasicSalary}
+            onChange={(e) => setLastBasicSalary(e.target.value)}
+            placeholder={"Enter your last basic salary"}
+            className={"body2 rounded-lg calculator-input"}
+          />
+          <Button
+            text="Calculate Retirement Amount"
+            onClick={handleRetirementCalculation}
+            className={"payroll-dashboard-view"}
+            textColor="text-white"
+          />
+          <Input
+            type={"text"}
+            name={"retiremenAmount"}
+            value={retirementAmount}
+            readonly
+            placeholder={"Enter your last basic salary"}
+            className={"body2 rounded-lg calculator-input"}
+          />{" "}
+        </div>
+        <div className="calculator-image-container">
+          <img src={accountingSvg} alt="" className="calaculator-image" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RetirementCalculator;
