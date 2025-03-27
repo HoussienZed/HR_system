7ac693874963f1c81f84ab13a86c06e39a739cd1
Houssien Zeineddine <houssienzd@hotmail.com>
2025-03-27 10:34:54 +0200

    Resolved an error: fixed configurations

--- Client/src/utils/axios.js
+++ Client/src/utils/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const axiosBaseUrl = axios.create({
-  // baseURL: "http://localhost:8000/api/v1",
-  baseURL: "http://52.47.74.242/snippet-server/api/v1",
+  baseURL: "http://13.37.211.34/hr-server/api/v1",
 
   headers: {
     "Content-Type": "application/json",
